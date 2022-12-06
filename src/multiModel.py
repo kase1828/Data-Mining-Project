ee4180d4a7869498f404610dfed3cece913f4016
willyt1200 <willyt1200@gmail.com>
2022-12-06 10:30:12 -0700

    More tuning on multiModel

--- src/multiModel.py
+++ src/multiModel.py
@@ -6,8 +6,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score
 from keras.models import Sequential, load_model
 from keras.layers import LSTM, Dense, Dropout
+from datetime import datetime
 
-def backtest(data, model, predictors, start=5, step=10):
+def backtest(data, model, predictors, start=7, step=2):
     predictions = []
     # Loop over the dataset in increments
     for i in range(start, data.shape[0], step):
@@ -75,6 +76,8 @@ def predictMulti(stockName, indv):
 
     print("Full data: ")
     print(data)
+    
+    
     full_predictors = predictors + ["weekly_mean", "quarterly_mean", "open_close_ratio", "high_close_ratio", "low_close_ratio", "weekly_trend"]
     ###
 
@@ -84,13 +87,13 @@ def predictMulti(stockName, indv):
     dataset_train = data.iloc[:-100]
     dataset_test = data.iloc[-100:]
 
-    model = RandomForestClassifier(n_estimators=200, min_samples_split=2, random_state=1)
+    model = RandomForestClassifier(n_estimators=300, min_samples_split=2, random_state=1)
 
-    ###
+    
     model.fit(dataset_train[model_predictors], dataset_train["Target"]) ##
-    ###
+    
 
-    #model.fit(dataset_train[predictors], dataset_train["Target"])
+    
 
     preds = model.predict(dataset_test[model_predictors]) ##
     preds = pd.Series(preds, index=dataset_test.index)
@@ -98,35 +101,11 @@ def predictMulti(stockName, indv):
 
     print(precision_score(dataset_test["Target"], preds))
 
-    #plt.show()
-    ###
+    
     predictions = backtest(data.iloc[90:], model, full_predictors)
-    ###
+    
 
-    #predictions = backtest(data, model, predictors)
-
-    # weekly_mean = data.rolling(7).mean()
-    # quarterly_mean = data.rolling(90).mean()
-    # weekly_trend = data.shift(1).rolling(7).mean()["Target"]
-
-    # data["weekly_mean"] = weekly_mean["close"] / data["close"]
-    # data["quarterly_mean"] = quarterly_mean["close"] / data["close"]
-
-    # data["weekly_trend"] = weekly_trend
-
-    # data["open_close_ratio"] = data["open"] / data["close"]
-    # data["high_close_ratio"] = data["high"] / data["close"]
-    # data["low_close_ratio"] = data["low"] / data["close"]
-
-
-    # print("Full data: ")
-    # print(data)
-    # full_predictors = predictors + ["weekly_mean", "quarterly_mean", "open_close_ratio", "high_close_ratio", "low_close_ratio", "weekly_trend"]
-
-    # predictions = backtest(data.iloc[90:], model, full_predictors)
-
-
-
+    
     print("Prediction percentage after backtesting:")
     print(precision_score(predictions["Target"], predictions["Predictions"]) * 100)
 
@@ -178,26 +157,27 @@ def predictMulti(stockName, indv):
         totalMoney += (shares * sell.iloc[-1,0])
         shares = 0
         trades += 1
-            
+    
 
+    startDate = np.datetime64(sell.index[0])
+    endDate = np.datetime64(sell.index[-1])
+
+    days = np.busday_count(startDate, endDate)
+
+    
+
+    print("Initial money: " + str(initialMoney))
     print("Total money: " + str(totalMoney))
     print("Total shares: " + str(shares))
     print("Total trades: " + str(trades))
     print("Percent gain or loss: " + str((totalMoney/initialMoney - 1) * 100) + '%')
+    print("Trading days: " + str(days))
 
     return(totalMoney - initialMoney)
 
 
     
-total = 0  
 
-#total += predictMulti('A', True)
-#total += predictMulti('AAPL', True)
-#total += predictMulti('AMD', True)
-#total += predictMulti('NVDA', True)
-#total += predictMulti('IBM', True)
-
-#print("Total profit: " + str(total))
 
 predictMulti('AMD', True)
 
